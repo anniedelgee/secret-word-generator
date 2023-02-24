d386336b8ff394d3a34e39649e113bc506db1dac
anniedelgee <117107188+anniedelgee@users.noreply.github.com>
2023-02-24 20:05:19 +0000

    functionality finally fixed

--- 04-password-generator-lesson/challenge/starter/script.js
+++ 04-password-generator-lesson/challenge/starter/script.js
@@ -89,160 +89,71 @@ var upperCasedCharacters = [
 ];
 
 
+var length;
+var lowercase;
+var uppercase;
+var special;
+var numeric;
 //global variables
 let userPreferences= [];
 // let passwordLength="";
-let password=[]
-
-
-// function generatePassword(){
-// defining passwordlength global variable 
-// function getLength(){
-  let passwordLength= prompt(
-    "how many characterz can you store in your head one time? between 10 & 64 plz.");
-      if (passwordLength < 10 || passwordLength > 64){
+let password;
+  let passwordLength = prompt("how many characterz can you store in your head one time? between 10 & 64 plz.");
+      if (passwordLength < 10 || passwordLength > 64)
+      {
          alert("not allowed, choose between 10 to 64 characters, mate");
-        let passwordLength= prompt("again, this time between 10 -64 characterz");
+        let passwordLength = prompt("again, this time between 10 -64 characterz");
       }
-    // }
-      // function getPreferences(){
-  //defining varibales to use in userPreferences if statement to get second global variable
-  //preferencesregarding each datatype are then passed into the concnatenated array r overall userPrerferences 
   let preferLow= confirm("What character types do you prefer? (select atleast 1): lowercase");
   let preferUpper= confirm("What character types do you prefer? (select atleast 1): uppercase");
   let preferNum= confirm("What character types do you prefer? (select atleast 1): numerical");
   let preferSpesh= confirm("What character types do you prefer? (select atleast 1): 'special' characters");
-    if (!preferLow && !preferUpper && !preferNum && !preferSpesh){
-          alert("wELLLLLLL, you have to choose atLEAST 1 charactertype, let's go again");{
-            preferLow= confirm("What character types do you prefer? (select atleast 1): lowercase");
-            preferUpper= confirm("What character types do you prefer?(select atleast 1): uppercase");
-            preferSpesh= confirm("What character types do you prefer? (select atleast 1): 'special' characters");
-          
-    }
+  if (!preferLow && !preferUpper && !preferNum && !preferSpesh)
+  {
+    alert("wELLLLLLL, you have to choose atLEAST 1 charactertype, let's go again");
+    preferLow= confirm("What character types do you prefer? (select atleast 1): lowercase");
+    preferUpper= confirm("What character types do you prefer?(select atleast 1): uppercase");
+    preferSpesh= confirm("What character types do you prefer? (select atleast 1): 'special' characters");
    }
-   //define userpreferences variable by combining all the preferences taken from confirm messages.
-  //  function getOptions(){
+function getOptions()
+{
     if (preferLow){
-      userPreferences= userPreferences.concat(lowerCasedCharacters);
+      userPreferences = userPreferences.concat(lowerCasedCharacters);
     }
      if (preferUpper){
-      userPreferences= userPreferences.concat(upperCasedCharacters);
+      userPreferences = userPreferences.concat(upperCasedCharacters);
     }
      if (preferNum){
-      userPreferences= userPreferences.concat(numericCharacters);
+      userPreferences = userPreferences.concat(numericCharacters);
     }
     if (preferSpesh){
-      userPreferences= userPreferences.concat(specialCharacters);
+      userPreferences = userPreferences.concat(specialCharacters);
     }
-  // }
-// }
-
+ }
   //function for  looping through array to form final randomly generated password to display on screen
-  function getRandom(arr){
-    return arr [Math.floor(Math.random()*arr.length)]
+  function getRandom(arr)
+  {
+    num = userPreferences[Math.floor(Math.random()*arr.length)]
+    return num
   }
-  
 function generatePassword(){
-  let possiblePassword= userPreferences.passwordLength;
-  // let openOptions= getPreferences();
-  //for loop to iterate randomly through userpreferences, constrained by passwordlegnth.
-    for (var i= 0; i < possiblePassword; i++ ){
-      let randomSelection= getRandom(possiblePassword);
-      let password= password.push(randomSelection);
+  getOptions()
+    for (var i= 0; i < passwordLength; i++ ){
+      let randomSelection = getRandom(userPreferences);
+      password += randomSelection;
     }
-    return password.join(" ");
+    return password;
 }
-
-
-  // let openOptions= getPreferences();
-  //   //for loop to iterate randomly through userpreferences, constrained by passwordlegnth.
-  //     for (var i= 0; i < possiblePassword; i++ ){
-  //       let randomSelection= getRandom(possiblePassword);
-  //       password.push(randomSelection);
-  //     }
-  //     return password.join("");
-
-
+writePassword()
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
-
 // // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  password = "";
+  generatePassword();
   var passwordText = document.querySelector('#password');
   passwordText.value = password;
+  console.log(password)
 }
 // // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
-
-// global varibales declared, so function has something to fill
-// let passwordLength="";
-// let userPreferences= [];
-// // let passwordLength=""
-// // let secretWord= userPreferences * passwordLength;
-
-// // function generatePassword 
-// // var topOfTopSecretWord= []
-// // let passwordLength=parseInt(passwordLength)
-
-// // Function to prompt user for password options
-// // function getPreferences() {
-//   //get length of password
-//   let passwordLength= prompt(
-//     "Choose between 10 - 64 characters for your secret word")
-//       if (passwordLength < 10 || passwordLength > 64){
-//       alert( "your secret word does not fit within our range, try again");
-//         let passwordLength= prompt( "Choose between 10 - 64 characters for your secret word")
-//       // generatePassword()
-//     }
-//     //represent characters as variables to get users preferences
-//     let preferLow= confirm("What character types do you prefer? (select atleast 1): lowercase");
-//     let preferUpper= confirm("What character types do you prefer? (select atleast 1): uppercase");
-//     let preferNum= confirm("What character types do you prefer? (select atleast 1): numerical");
-//     let preferSpesh= confirm("What character types do you prefer? (select atleast 1): 'special' characters");
-//       if (!preferLow && !preferUpper && !preferNum && !preferSpesh){
-//         alert("wELLLLLLL, you have to choose atLEAST 1 charactertype, let's go again");{
-//           preferLow= confirm("What character types do you prefer? (select atleast 1): lowercase");
-//           preferUpper= confirm("What character types do you prefer?(select atleast 1): uppercase");
-//           preferSpesh= confirm("What character types do you prefer? (select atleast 1): 'special' characters");
-//         }
-//       //use confirmed user preferences to create concatenated array of characters to randomly iterate through. 
-//       let userPreferences= [];
-//       if (preferLow){
-//         userPreferences= userPreferences.concat(lowerCasedCharacters);
-//       }
-//       if (preferUpper){
-//         userPreferences= userPreferences.concat(upperCasedCharacters);
-//       }
-//       if (preferNum){
-//         userPreferences= userPreferences.concat(numericCharacters);
-//       }
-//       if (preferSpesh){
-//         userPreferences= userPreferences.concat(specialCharacters);
-//       }
-//     }
-//     // console.log(userPreferences);
-//   // }
-//   // generatePassword();
-//   // randomly iterate through concatenated array.
-//   let topOfTopSecretWord = []
-//   function generatePassword(){
-//     for (var i=0; i< passwordLength; i++){
-//       let finalCharacters= userPreferences[Math.floor(Math.random()* userPreferences.length)];
-//         topOfTopSecretWord.push(finalCharacters);
-//       }
-//       return topOfTopSecretWord.join("");
-//     }
-// //        //string that joins elements in array into a single string without commas etc.
-  
-// Get references to the #generate element
-// var generateBtn = document.querySelector('#generate');
-
-// // // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector('#password');
-//   passwordText.value = password;
-// }
-// // Add event listener to generate button
-// generateBtn.addEventListener('click', writePassword)
